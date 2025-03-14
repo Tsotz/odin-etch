10858a323b21998356024049843b356bb70be2d8
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-14 17:47:53 +0300

    functionailty done flavor remains

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const cont = document.querySelector("#container")
-
 const grid = document.createElement("div");
 grid.classList.add("grid");
-grid.setAttribute("style", "display:flex; flex-wrap:wrap; width:400px; height:400px; border:2px solid black;");
 cont.appendChild(grid);
 
-for (let i = 0; i < 16 * 16; i++) {
+
+
+
+
+
+const but = document.querySelector("button")
+
+but.addEventListener("click",function(){
+    
+   const size = prompt("Please choose a grid size\nMust be etween 0-100")
+   if (size < 0 || size > 100 ) return;
+
+    grid.innerHTML = "";
+
+   for (let i = 0; i < size * size; i++) {
 
     const square = document.createElement("div");
     square.classList.add("square");
@@ -14,5 +26,31 @@ for (let i = 0; i < 16 * 16; i++) {
     square.style.border = "1px solid gray";
     square.style.boxSizing = "border-box";
     grid.appendChild(square);
+    
 
 }
+
+const squares = document.querySelectorAll(".square")
+
+squares.forEach(function(square){
+
+    square.addEventListener("mouseover", function(){
+
+        square.style.backgroundColor = "gray";
+    
+    });
+
+})
+
+grid.style.display = "flex";
+grid.style.flexWrap = "wrap";
+grid.style.flexShrink = "0";
+grid.style.width = `${25 * size}px`; 
+grid.style.height = `${25 * size}px`; 
+grid.style.border = "2px solid black";
+
+})
+
+
+
+
