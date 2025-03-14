4d4e7c17989de3c5e0a18b3a294920d66fd0a783
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-14 20:07:55 +0300

    all done

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ cont.appendChild(grid);
 
 
 
+let mode = "color";
+
+const color = document.querySelector("#color");
+const dark = document.querySelector("#dark");
 
 
+color.addEventListener("click", function(){ mode = "color";})
+dark.addEventListener("click", function(){ mode = "dark";})
 
-const but = document.querySelector("button")
+
+const but = document.querySelector(".buttons")
 
 but.addEventListener("click",function(){
     
@@ -27,30 +34,78 @@ but.addEventListener("click",function(){
     square.style.boxSizing = "border-box";
     grid.appendChild(square);
     
-
 }
 
 const squares = document.querySelectorAll(".square")
 
 squares.forEach(function(square){
 
-    square.addEventListener("mouseover", function(){
+    square.addEventListener("mouseover", function(){  // if you pull out the if statement, squares changes colors always
+        
+        if (mode === "color"){
+        if (!square.style.backgroundColor){
 
-        square.style.backgroundColor = "gray";
-    
+        square.style.backgroundColor = `${randomColor()}`;
+        
+        }
+        }
+        if (mode === "dark") {
+
+            if (!square.style.backgroundColor){
+
+                square.style.backgroundColor = "black";
+                square.style.opacity = `${darkerSq()}`;
+                }
+
+        }
+
+
+
+
     });
 
+    })
+
+        grid.style.display = "flex";
+        grid.style.flexWrap = "wrap";
+        grid.style.flexShrink = "0";
+        grid.style.width = `${25 * size}px`; 
+        grid.style.height = `${25 * size}px`; 
+        grid.style.border = "2px solid black";
+
 })
 
-grid.style.display = "flex";
-grid.style.flexWrap = "wrap";
-grid.style.flexShrink = "0";
-grid.style.width = `${25 * size}px`; 
-grid.style.height = `${25 * size}px`; 
-grid.style.border = "2px solid black";
 
-})
+const randomColor = function() {
+
+    let id = "#";
+
+    for (i=0; i < 6; i++){
+
+        const ranHex = Math.floor(Math.random() * 16).toString(16);
+        id += ranHex;
+        
+    }
+
+    return id;
+    
+    
+}
+
+const numList = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+let index = 0;
+
+const darkerSq = function () {
+
+    if (index < numList.length) {
+
+        let opacity = numList[index];
+        index++;
+        return opacity.toString();
+
+    } else {return 1;}
 
 
 
+}
 
